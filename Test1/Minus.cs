4eedb7b78062839cd0c363f153056db4cb72ba52
stasbebra2006 <liashevskiys@gmail.com>
2022-11-09 10:41:16 +0100

    Test1

---
+++ Test1/Minus.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace Test1
+{
+    public class Minus
+    {
+        public void app()
+        {
+            bool processed = true;
+            while (processed)
+            {
+                int cislo;
+                Console.WriteLine(" Zadej číslo");
+                cislo = SafelyConvertToInt(Console.ReadLine());
+                if (cislo == -1000)
+                {
+                    processed = false;
+                }
+                else
+                {
+                    cislo = cislo - 5000;
+                    Console.WriteLine(cislo);
+                }
+
+
+            }
+
+            
+        }
+        public int SafelyConvertToInt(string x)
+        {
+            if (int.TryParse(x, out int result))
+            {
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Test1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Minus minus = new Minus();
+            minus.app();
+        }
+    }
+}
