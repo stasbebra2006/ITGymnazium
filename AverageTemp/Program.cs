1f571ee2ed8932491fad44286f63ca88b194f8dd
stasbebra2006 <liashevskiys@gmail.com>
2022-11-22 09:05:36 +0100

    TempApp

---
+++ AverageTemp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AverageTemp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TempApp tempApp = new TempApp();
+            tempApp.Run();
+        }
+    }
+}
---
+++ AverageTemp/TempApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageTemp
+{
+    public class TempApp
+    {
+        public void Run()
+        {
+            double tempSum = 0;
+            double temp;
+            double i = 0;
+            bool processed = true;
+            while (processed)
+            {
+                Console.WriteLine("Write temp");
+                temp = Convert.ToDouble(Console.ReadLine());
+                if(temp == int.MinValue)
+                {
+                    processed = false;
+                }
+                else
+                {
+                    tempSum += temp;
+                    i++;
+                }
+            }
+            double averageTemp = Math.Round(tempSum / i, 1);
+            Console.WriteLine($"averageTemp = {averageTemp}");
+        }
+    }
+}
